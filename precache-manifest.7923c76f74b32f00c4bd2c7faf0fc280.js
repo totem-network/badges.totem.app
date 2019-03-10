d70668ae0b0eec6e4a489d840ab3b76ef61c8267
Daniel Wagner <daniel.wagner@totem.network>
2019-03-10 18:08:17 +0100

    initial commit

---
+++ precache-manifest.7923c76f74b32f00c4bd2c7faf0fc280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0f9ec106237e180ee51f",
+    "url": "/static/js/main.3992f10a.chunk.js"
+  },
+  {
+    "revision": "68faf642f2828899940e",
+    "url": "/static/js/2.3849b622.chunk.js"
+  },
+  {
+    "revision": "0f9ec106237e180ee51f",
+    "url": "/static/css/main.82004412.chunk.css"
+  },
+  {
+    "revision": "f7dd87b61e3d7077ea859da3e8361181",
+    "url": "/index.html"
+  }
+];
